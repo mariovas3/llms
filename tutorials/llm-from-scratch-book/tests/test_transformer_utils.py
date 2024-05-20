7db22682a910de6169fc4886b8d122e81d7e5da8
Mario Vasilev <mariovasilev212@gmail.com>
2024-05-20 13:44:47 +0100

    add test for mha with one fewer transpose operation

--- tutorials/llm-from-scratch-book/tests/test_transformer_utils.py
+++ tutorials/llm-from-scratch-book/tests/test_transformer_utils.py
@@ -41,3 +41,24 @@ def test_parallel_mha_with_bias():
     out2 = mha(tgt, memory, tgt_mask)
     # test all close;
     assert torch.allclose(out1, out2)
+
+
+def test_parallel_mha():
+    tgt = torch.randn(3, 5, 8)
+    memory = torch.randn(3, 7, 8)
+    qkv_bias = True
+    tgt_mask = utils.get_subsequent_mask(7)[: tgt.size(-2), : memory.size(-2)]
+
+    naive_mha = tu.MultiHeadAttSlower(
+        d_model=8, num_heads=2, qkv_bias=qkv_bias
+    )
+    out1 = naive_mha(tgt, memory, tgt_mask)
+    assert out1.shape == tgt.shape
+    mha = utils.MultiHeadAtt(d_model=8, num_heads=2, qkv_bias=qkv_bias)
+    # make sure I have the same weights;
+    for key, param in mha.named_parameters():
+        param.data = naive_mha.get_parameter(key).data
+    # predict
+    out2 = mha(tgt, memory, tgt_mask)
+    # test all close;
+    assert torch.allclose(out1, out2)
